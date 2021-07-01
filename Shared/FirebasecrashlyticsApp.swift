7a21758390094cd3b77d19d3a55a840b0949c2f7
iOS <>
2021-07-01 23:18:31 +0530

    - init code

---
+++ Shared/FirebasecrashlyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasecrashlyticsApp.swift
+//  Shared
+//
+//  Created by iOS on 01/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebasecrashlyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
